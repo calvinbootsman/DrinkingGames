92ebd087bf1eb0012ad526c839b7fce91749e85f
Calvin Bootsman <calvinbootsman@hotmail.com>
2017-07-09 00:38:37 +0300

    First Commit

---
+++ DrinkingGames/DrinkingGamesPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace DrinkingGames
+{
+    public partial class DrinkingGamesPage : ContentPage
+    {
+        int NumberOfPlayers = 1;
+        Entry[] entry = new Entry[12];
+        public DrinkingGamesPage()
+        {
+            InitializeComponent();
+            for (NumberOfPlayers =1; NumberOfPlayers <= 4; NumberOfPlayers++){
+                entry[NumberOfPlayers] = new Entry {Placeholder="Player "+ NumberOfPlayers.ToString() };
+                PlayersLayout.Children.Add(entry[NumberOfPlayers]);
+            }
+
+        }
+
+        void AddPlayer(object sender, System.EventArgs e)
+        {
+            NumberOfPlayers++;
+			entry[NumberOfPlayers] = new Entry { Placeholder = "Player " + NumberOfPlayers.ToString() };
+			PlayersLayout.Children.Add(entry[NumberOfPlayers]);
+        }
+    }
+}
